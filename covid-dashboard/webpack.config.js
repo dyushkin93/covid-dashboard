2edad33282e73a3359e96c298d3cbb8545eb6c74
dyushkin93 <d.yushkin93@gmail.com>
2020-12-06 20:16:19 +0500

    init: create project structure and dependencies

---
+++ covid-dashboard/webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './src/js/index.js'),
+  },
+
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/template.html'),
+      filename: 'index.html',
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+
+  module: {
+    rules: [
+      {
+      test: /\.js$/,
+      exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+
+      {
+      test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+      type: 'asset/resource',
+      },
+
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+
+      {
+        test: /\.(scss|css)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+
+      {
+        test: /^(?!.*template).*\.html.*$/i, // all html except of template
+        loader: 'html-loader',
+      }
+    ]
+  },
+
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, './dist'),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
+  },
+}
