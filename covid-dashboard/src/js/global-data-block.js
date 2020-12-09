9af1954e2a102e91060472441e30576b43678225
vio7enc3 <70806255+vio7enc3@users.noreply.github.com>
2020-12-09 15:01:47 +0500

    fix: fix quotes

--- covid-dashboard/src/js/global-data-block.js
+++ covid-dashboard/src/js/global-data-block.js
@@ -4,29 +4,29 @@ export default class GlobalDataBlock {
     this.population100k = population;
 
     // elements where data should be exported
-    this.casesElem = document.querySelector("body");
-    this.deathsElem = document.querySelector("body");
-    this.recoveredElem = document.querySelector("body");
+    this.casesElem = document.querySelector('body');
+    this.deathsElem = document.querySelector('body');
+    this.recoveredElem = document.querySelector('body');
   }
 
   getCases(period, units) {
     let cases;
     let deaths;
     let recovered;
-    if (period === "total" && units === "absolute") {
-      console.log(this.globalData)
+    if (period === 'total' && units === 'absolute') {
+      console.log(this.globalData);
       cases = this.globalData.confirmed;
       deaths = this.globalData.deaths;
       recovered = this.globalData.recovered;
-    } else if (period === "lastDay" && units === "absolute") {
+    } else if (period === 'lastDay' && units === 'absolute') {
       cases = this.globalData.new_confirmed;
       deaths = this.globalData.new_deaths;
       recovered = this.globalData.new_recovered;
-    } else if (period === "total" && units === "relavite") {
+    } else if (period === 'total' && units === 'relavite') {
       cases = parseInt((this.globalData.confirmed / this.population100k), 10);
       deaths = parseInt((this.globalData.deaths / this.population100k), 10);
       recovered = parseInt((this.globalData.recovered / this.population100k), 10);
-    } else if (period === "lastDay" && units === "relavite") {
+    } else if (period === 'lastDay' && units === 'relavite') {
       cases = parseInt((this.globalData.new_confirmed / this.population100k), 10);
       deaths = parseInt((this.globalData.new_deaths / this.population100k), 10);
       recovered = parseInt((this.globalData.new_recovered / this.population100k), 10);
@@ -36,6 +36,4 @@ export default class GlobalDataBlock {
     this.deathsElem.innerHTML = deaths;
     this.recoveredElem.innerHTML = recovered;
   }
-
-  
 }
