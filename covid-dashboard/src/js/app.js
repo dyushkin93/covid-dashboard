0fdee1e3ccb42787fc7613c47c5e5ef4b8c5e825
vio7enc3 <70806255+vio7enc3@users.noreply.github.com>
2020-12-10 20:02:07 +0500

    fix: fix cases block render

--- covid-dashboard/src/js/app.js
+++ covid-dashboard/src/js/app.js
@@ -1,7 +1,6 @@
 import GlobalDataBlock from './global-data-block';
 import CovidChart from './covid-chart';
 import CasesByCountry from './cases-by-contry';
-import TypesAndUnits from './switcher';
 
 export default class App {
   constructor(globalData, byCountryData, byCountryTimeline) {
@@ -9,6 +8,12 @@ export default class App {
 
     this.byCountryData = byCountryData.data;
     this.byCountryTimeline = byCountryTimeline;
+
+    this.cases = document.getElementById('cases');
+    this.death = document.getElementById('death');
+    this.recovered = document.getElementById('recovered');
+
+    this.type = 'cases';
 
     this.globalPopulation100k = 7700000000 / 100000;
     // period to show in blocks "total" || "lastDay"
@@ -19,10 +24,9 @@ export default class App {
     console.log(this.byCountryTimeline);
 
     // units to show in byCountryCases block
-    this.type = new TypesAndUnits().getType();
-    console.log('type is ' + this.type);
+
     // initialisation of CasesByCountry block
-    this.casesByCountry = new CasesByCountry(byCountryData).createCountryCases(this.type);
+    this.casesByCountry = new CasesByCountry(byCountryData).updateCountryCases();
 
     // initialisation of GlobalData block
     // this.globalDataBlock = new GlobalDataBlock(this.globalData, this.globalPopulation100k);
--- covid-dashboard/src/js/cases-by-contry.js
+++ covid-dashboard/src/js/cases-by-contry.js
@@ -8,31 +8,102 @@ export default class CasesByCountry {
     this.cases = document.getElementById('cases');
     this.death = document.getElementById('death');
     this.recovered = document.getElementById('recovered');
+
+    this.switcher = document.getElementById('switcher');
+    this.mapSwitcher = document.getElementById('map-switcher');
+    this.deskBoardSwitcher = document.getElementById('deskboard-switcher');
+    this.unitsType = document.getElementById('units');
+
+    this.type = 'cases';
+    this.units = 'absolute';
   }
 
-  createCountryCases(type) {
-    if (type === 'death') {
-      console.log(type);
-      this.countryBlock.innerHTML = '';
-      this.byCountryData.forEach((el) => {
-        this.countryCases = create('div', 'confirmed', `${el.latest_data.deaths}`);
-        this.country = create('div', 'country', `${el.name}`);
-        this.countryCell = create('div', 'country-cell', [this.countryCases, this.country], this.countryBlock, ['data-key', `${el.code}`]);
-      });
-    } else if (type === 'recovered') {
-      console.log(type);
-      this.countryBlock.innerHTML = '';
-      this.byCountryData.forEach((el) => {
-        this.countryCases = create('div', 'confirmed', `${el.latest_data.recovered}`);
-        this.country = create('div', 'country', `${el.name}`);
-        this.countryCell = create('div', 'country-cell', [this.countryCases, this.country], this.countryBlock, ['data-key', `${el.code}`]);
-      });
-    } else console.log(type);
-    this.countryBlock.innerHTML = '';
-    this.byCountryData.forEach((el) => {
-      this.countryCases = create('div', 'confirmed', `${el.latest_data.confirmed}`);
-      this.country = create('div', 'country', `${el.name}`);
-      this.countryCell = create('div', 'country-cell', [this.countryCases, this.country], this.countryBlock, ['data-key', `${el.code}`]);
+  createCountryCases(type, units) {
+    if (units === 'absolute') {
+      if (type === 'death') {
+        this.countryBlock.innerHTML = '';
+        this.byCountryData.sort((a, b) => b.latest_data.deaths - a.latest_data.deaths)
+          .forEach((el) => {
+            this.countryCases = create('div', 'confirmed', `${el.latest_data.deaths}`);
+            this.country = create('div', 'country', `${el.name}`);
+            this.countryCell = create('div', 'country-cell', [this.countryCases, this.country], this.countryBlock, ['data-key', `${el.code}`]);
+          });
+      } else if (type === 'recovered') {
+        this.countryBlock.innerHTML = '';
+        this.byCountryData.sort((a, b) => b.latest_data.recovered - a.latest_data.recovered)
+          .forEach((el) => {
+            this.countryCases = create('div', 'confirmed', `${el.latest_data.recovered}`);
+            this.country = create('div', 'country', `${el.name}`);
+            this.countryCell = create('div', 'country-cell', [this.countryCases, this.country], this.countryBlock, ['data-key', `${el.code}`]);
+          });
+      } else this.countryBlock.innerHTML = '';
+      this.byCountryData.sort((a, b) => b.latest_data.confirmed - a.latest_data.confirmed)
+        .forEach((el) => {
+          this.countryCases = create('div', 'confirmed', `${el.latest_data.confirmed}`);
+          this.country = create('div', 'country', `${el.name}`);
+          this.countryCell = create('div', 'country-cell', [this.countryCases, this.country], this.countryBlock, ['data-key', `${el.code}`]);
+        });
+    } else if (units === 'relative') {
+      if (type === 'death') {
+        this.countryBlock.innerHTML = '';
+        this.byCountryData.sort((a, b) => b.latest_data.deaths - a.latest_data.deaths)
+          .forEach((el) => {
+            this.countryCases = create('div', 'confirmed', `${el.latest_data.deaths !== 0 ? Math.round((el.latest_data.deaths / el.population) * 100000, -2) : 0}`);
+            this.country = create('div', 'country', `${el.name}`);
+            this.countryCell = create('div', 'country-cell', [this.countryCases, this.country], this.countryBlock, ['data-key', `${el.code}`]);
+          });
+      } else if (type === 'recovered') {
+        this.countryBlock.innerHTML = '';
+        this.byCountryData.sort((a, b) => b.latest_data.deaths - a.latest_data.deaths)
+          .forEach((el) => {
+            this.countryCases = create('div', 'confirmed', `${el.latest_data.recovered !== 0 ? Math.round((el.latest_data.recovered / el.population) * 100000, -2) : 0}`);
+            this.country = create('div', 'country', `${el.name}`);
+            this.countryCell = create('div', 'country-cell', [this.countryCases, this.country], this.countryBlock, ['data-key', `${el.code}`]);
+          });
+      } else this.countryBlock.innerHTML = '';
+      this.byCountryData.sort((a, b) => b.latest_data.deaths - a.latest_data.deaths)
+        .forEach((el) => {
+          this.countryCases = create('div', 'confirmed', `${el.latest_data.confirmed !== 0 ? Math.round((el.latest_data.confirmed / el.population) * 100000, -2) : 0}`);
+          this.country = create('div', 'country', `${el.name}`);
+          this.countryCell = create('div', 'country-cell', [this.countryCases, this.country], this.countryBlock, ['data-key', `${el.code}`]);
+        });
+    }
+  }
+
+  updateCountryCases() {
+    this.createCountryCases(this.type, this.units);
+    this.switcher.addEventListener('click', (e) => {
+      if (e.target.id === this.cases.id) {
+        this.type = 'cases';
+        this.createCountryCases(this.type, this.units);
+      } else if (e.target.id === this.death.id) {
+        this.type = 'death';
+        this.createCountryCases(this.type, this.units);
+      } else if (e.target.id === this.recovered.id) {
+        this.type = 'recovered';
+        this.createCountryCases(this.type, this.units);
+      }
+    });
+    this.mapSwitcher.addEventListener('click', (e) => {
+      if (e.target.id === this.cases.id) {
+        this.type = 'cases';
+        this.createCountryCases(this.type);
+      } else if (e.target.id === this.death.id) {
+        this.type = 'death';
+        this.createCountryCases(this.type);
+      } else if (e.target.id === this.recovered.id) {
+        this.type = 'recovered';
+        this.createCountryCases(this.type);
+      }
+    });
+    this.deskBoardSwitcher.addEventListener('click', (e) => {
+      if (e.target.id === this.unitsType.id && this.units === 'absolute') {
+        this.units = 'relative';
+        return this.createCountryCases(this.type, this.units);
+      } if (e.target.id === this.unitsType.id && this.units === 'relative') {
+        this.units = 'absolute';
+        return this.createCountryCases(this.type, this.units);
+      }
     });
   }
 }
