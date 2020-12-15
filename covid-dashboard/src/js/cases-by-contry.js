f450da4815147ff4c17eab9f2736905db383df7f
vio7enc3 <70806255+vio7enc3@users.noreply.github.com>
2020-12-16 02:21:37 +0500

    fix: change api

--- covid-dashboard/src/js/cases-by-contry.js
+++ covid-dashboard/src/js/cases-by-contry.js
@@ -1,8 +1,8 @@
 import create from './helpers/create';
 
 export default class CasesByCountry {
-  constructor(byCountryData) {
-    this.byCountryData = byCountryData.data;
+  constructor(covidData) {
+    this.byCountryData = Object.values(covidData).slice(1);
 
     this.countryBlock = document.getElementById('by-country');
     this.cases = document.getElementById('cases');
@@ -45,9 +45,9 @@ export default class CasesByCountry {
     if (units === 'absolute') {
       if (type === 'death') {
         this.countryBlock.innerHTML = '';
-        data.sort((a, b) => b.latest_data.deaths - a.latest_data.deaths)
+        data.sort((a, b) => b.deaths - a.deaths)
           .forEach((el) => {
-            this.countryCases = create('div', 'confirmed', `${el.latest_data.deaths}`);
+            this.countryCases = create('div', 'confirmed', `${el.deaths}`);
             this.country = create('div', 'country', `${el.name}`);
             this.flag = create('img', 'flag', null, null, ['src', `../assets/img/flags/${el.code.toLowerCase()}.svg`], ['alt', `${el.code}`]);
             this.countryCell = create('div', 'country-cell', [this.countryCases, this.country, this.flag], this.countryBlock, ['data-key', `${el.code}`]);
@@ -55,18 +55,18 @@ export default class CasesByCountry {
         return this.countryCell;
       } if (type === 'recovered') {
         this.countryBlock.innerHTML = '';
-        data.sort((a, b) => b.latest_data.recovered - a.latest_data.recovered)
+        data.sort((a, b) => b.recovered - a.recovered)
           .forEach((el) => {
-            this.countryCases = create('div', 'confirmed', `${el.latest_data.recovered}`);
+            this.countryCases = create('div', 'confirmed', `${el.recovered}`);
             this.country = create('div', 'country', `${el.name}`);
             this.flag = create('img', 'flag', null, null, ['src', `../assets/img/flags/${el.code.toLowerCase()}.svg`], ['alt', `${el.code}`]);
             this.countryCell = create('div', 'country-cell', [this.countryCases, this.country, this.flag], this.countryBlock, ['data-key', `${el.code}`]);
           });
         return this.countryCell;
       } this.countryBlock.innerHTML = '';
-      data.sort((a, b) => b.latest_data.confirmed - a.latest_data.confirmed)
+      data.sort((a, b) => b.cases - a.cases)
         .forEach((el) => {
-          this.countryCases = create('div', 'confirmed', `${el.latest_data.confirmed}`);
+          this.countryCases = create('div', 'confirmed', `${el.cases}`);
           this.country = create('div', 'country', `${el.name}`);
           this.flag = create('img', 'flag', null, null, ['src', `../assets/img/flags/${el.code.toLowerCase()}.svg`], ['alt', `${el.code}`]);
           this.countryCell = create('div', 'country-cell', [this.countryCases, this.country, this.flag], this.countryBlock, ['data-key', `${el.code}`]);
@@ -75,9 +75,9 @@ export default class CasesByCountry {
     } if (units === 'relative') {
       if (type === 'death') {
         this.countryBlock.innerHTML = '';
-        data.sort((a, b) => b.latest_data.deaths - a.latest_data.deaths)
+        data.sort((a, b) => b.deaths - a.deaths)
           .forEach((el) => {
-            this.countryCases = create('div', 'confirmed', `${el.latest_data.deaths !== 0 ? Math.round((el.latest_data.deaths / el.population) * 100000, -2) : 0}`);
+            this.countryCases = create('div', 'confirmed', `${el.deaths !== 0 ? Math.round((el.deaths / el.population) * 100000, -2) : 0}`);
             this.country = create('div', 'country', `${el.name}`);
             this.flag = create('img', 'flag', null, null, ['src', `../assets/img/flags/${el.code.toLowerCase()}.svg`], ['alt', `${el.code}`]);
             this.countryCell = create('div', 'country-cell', [this.countryCases, this.country, this.flag], this.countryBlock, ['data-key', `${el.code}`]);
@@ -85,18 +85,18 @@ export default class CasesByCountry {
         return this.countryCell;
       } if (type === 'recovered') {
         this.countryBlock.innerHTML = '';
-        data.sort((a, b) => b.latest_data.deaths - a.latest_data.deaths)
+        data.sort((a, b) => b.deaths - a.deaths)
           .forEach((el) => {
-            this.countryCases = create('div', 'confirmed', `${el.latest_data.recovered !== 0 ? Math.round((el.latest_data.recovered / el.population) * 100000, -2) : 0}`);
+            this.countryCases = create('div', 'confirmed', `${el.recovered !== 0 ? Math.round((el.recovered / el.population) * 100000, -2) : 0}`);
             this.country = create('div', 'country', `${el.name}`);
             this.flag = create('img', 'flag', null, null, ['src', `../assets/img/flags/${el.code.toLowerCase()}.svg`], ['alt', `${el.code}`]);
             this.countryCell = create('div', 'country-cell', [this.countryCases, this.country, this.flag], this.countryBlock, ['data-key', `${el.code}`]);
           });
         return this.countryCell;
       } this.countryBlock.innerHTML = '';
-      data.sort((a, b) => b.latest_data.deaths - a.latest_data.deaths)
+      data.sort((a, b) => (b.cases / b.population) * 100000 - (a.cases / a.population) * 100000)
         .forEach((el) => {
-          this.countryCases = create('div', 'confirmed', `${el.latest_data.confirmed !== 0 ? Math.round((el.latest_data.confirmed / el.population) * 100000, -2) : 0}`);
+          this.countryCases = create('div', 'confirmed', `${el.cases !== 0 ? Math.round((el.cases / el.population) * 100000, -2) : 0}`);
           this.country = create('div', 'country', `${el.name}`);
           this.flag = create('img', 'flag', null, null, ['src', `../assets/img/flags/${el.code.toLowerCase()}.svg`], ['alt', `${el.code}`]);
           this.countryCell = create('div', 'country-cell', [this.countryCases, this.country, this.flag], this.countryBlock, ['data-key', `${el.code}`]);
