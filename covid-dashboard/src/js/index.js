1f83feaf24a453e3e68f6c19256078342c1a007a
vio7enc3 <70806255+vio7enc3@users.noreply.github.com>
2020-12-16 01:26:17 +0500

    feat: add bootstrap

--- covid-dashboard/src/js/index.js
+++ covid-dashboard/src/js/index.js
@@ -1,5 +1,4 @@
-import 'materialize-css/dist/css/materialize.min.css';
-import M from 'materialize-css/dist/js/materialize.min';
+import 'bootswatch/dist/darkly/bootstrap.min.css';
 import 'normalize-css';
 import create from './helpers/create';
 import '../scss/style.scss';
